5a08dafd07585a86a23abacda0af2ef89341925f
lihaoyang27 <102766172+lihaoyang27@users.noreply.github.com>
2022-09-03 21:59:54 -0400

    first commit

---
+++ src/HOC/PaymentPageLayout/PaymentPageLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './paymentPageLayout.scss'
+import FormContainer from "../FormContainer/FormContainer";
+
+function PaymentPageLayout() {
+    return (
+        <div>
+            pagelayout
+            <FormContainer/>
+        </div>
+    );
+}
+
+export default PaymentPageLayout;
